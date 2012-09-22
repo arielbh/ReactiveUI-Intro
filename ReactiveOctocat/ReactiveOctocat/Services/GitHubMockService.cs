5fe29d0e0bb1405c99ef3d538fc2d95bb1f07895
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-22 22:47:46 +0300

    Added Mock Service

---
+++ ReactiveOctocat/ReactiveOctocat/Services/GitHubMockService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ReactiveOctocat.Services
+{
+    internal class GitHubMockService : IGitHubService
+    {
+        private int _current = 1;
+        Dictionary<int, Repository[]> _repositories = new Dictionary<int, Repository[]>
+        {
+            {1, new []
+            {
+                new Repository { Name = "jQuery", Language = "JavaScript"},
+                new Repository { Name = "ReactiveUI", Language = "C#"},
+                new Repository { Name = "Bootsrap", Language = "JavaScript"},
+                new Repository { Name = "Homebrew", Language = "Ruby"},
+            }},
+                {2, new []
+            {
+                new Repository { Name = "Rails", Language = "Ruby"},
+                new Repository { Name = "html5-boilerplate", Language = "JavaScript"},
+                new Repository { Name = "SuiteValue.UI.Metro", Language = "C#"},
+                new Repository { Name = "Node", Language = "JavaScript"},
+            }},
+                {3, new []
+            {
+                new Repository { Name = "ImpressJS", Language = "JavaScript"},
+                new Repository { Name = "CodeCommander", Language = "C#"},
+                new Repository { Name = "Backbone", Language = "JavaScript"},
+                new Repository { Name = "Linux", Language = "C"},
+            }}
+        };
+
+        public User Login(string userName, string password)
+        {
+            Thread.Sleep(1000);
+            return new User {Name = userName};
+        }
+
+        public Repository[] GetRepositories(User user)
+        {
+            _current++;
+            if (_current == 4) _current = 1;
+            Thread.Sleep(4000);
+            return _repositories[_current];
+        }
+    }
+
+    internal interface IGitHubService
+    {
+        User Login(string userName, string password);
+        Repository[] GetRepositories(User user);
+    }
+
+    internal class Repository
+    {
+        public string Name { get; set; }
+        public string Language { get; set; }
+    }
+
+    internal class User
+    {
+        public string Name { get; set; }
+    }
+
+}
---
+++ ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveOctocat.Services;
+
+namespace ReactiveOctocat.ViewModels
+{
+    class MainViewModel
+    {
+        private IGitHubService _gitHubService = new GitHubMockService();
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
