8fe83da4607e83951c474f055ed4504057996990
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-22 23:36:39 +0300

    Added UserName/Password properties

--- ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
+++ ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ReactiveOctocat.Services;
+using ReactiveUI;
 
 namespace ReactiveOctocat.ViewModels
 {
-    class MainViewModel 
+    class MainViewModel : ReactiveObject
     {
         private IGitHubService _gitHubService = new GitHubMockService();
 
         public MainViewModel()
         {
-            
+
+
         }
+
+        private string _UserName;
+
+        public string UserName
+        {
+            get { return _UserName; }
+            set { this.RaiseAndSetIfChanged(x => x.UserName, value);
+            }
+        }
+
+        private string _Password;
+
+        public string Password
+        {
+            get { return _Password; }
+            set { this.RaiseAndSetIfChanged(x => x.Password, value); }
+        }
+
     }
 }
