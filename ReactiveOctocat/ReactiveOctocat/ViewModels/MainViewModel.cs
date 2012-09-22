9ed7835d3bd506c425e3131747d13a8ff9d6aa9e
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-22 23:08:33 +0300

    OK Now we got Reactive Packages for real

--- ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
+++ ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
@@ -6,7 +6,7 @@ using ReactiveOctocat.Services;
 
 namespace ReactiveOctocat.ViewModels
 {
-    class MainViewModel
+    class MainViewModel 
     {
         private IGitHubService _gitHubService = new GitHubMockService();
 
