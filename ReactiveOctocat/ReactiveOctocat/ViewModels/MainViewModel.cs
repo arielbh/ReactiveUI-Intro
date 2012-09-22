1e1e0b31d822e34005156d5c6cc0c02bfd2e4ca3
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-22 23:55:52 +0300

    Added LoginCommand

--- ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
+++ ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 using ReactiveOctocat.Services;
 using ReactiveUI;
+using ReactiveUI.Xaml;
 
 namespace ReactiveOctocat.ViewModels
 {
@@ -14,7 +17,11 @@ namespace ReactiveOctocat.ViewModels
         public MainViewModel()
         {
 
+            LoginCommand = new ReactiveCommand(this.WhenAny(t => t.UserName, t => t.Password, (x, y) => !string.IsNullOrEmpty(x.Value) && !string.IsNullOrEmpty(y.Value)));
+            LoginCommand.Subscribe(_ => LoggedInUser = _gitHubService.Login(UserName, Password));
 
+            this.ObservableForProperty(x => x.LoggedInUser,
+                                       user => user == null ? Visibility.Hidden : Visibility.Visible).Subscribe(v => IsUserLoggedIn = v);
         }
 
         private string _UserName;
@@ -33,6 +40,26 @@ namespace ReactiveOctocat.ViewModels
             get { return _Password; }
             set { this.RaiseAndSetIfChanged(x => x.Password, value); }
         }
+
+        private User _LoggedInUser;
+
+        public User LoggedInUser
+        {
+            get { return _LoggedInUser; }
+            set { this.RaiseAndSetIfChanged(x => x.LoggedInUser, value); }
+        }
+
+        private Visibility _IsUserLoggedIn;
+
+        public Visibility IsUserLoggedIn
+        {
+            get { return _IsUserLoggedIn; }
+            set { this.RaiseAndSetIfChanged(x => x.IsUserLoggedIn, value); }
+        }
+
+
+
+        public ReactiveCommand LoginCommand { get; set; }
 
     }
 }
