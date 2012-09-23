f1837bd608312fbcc2b02b9ee5a6f504f1e7346d
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-23 13:35:56 +0200

    Step 5a better MRU

--- ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
+++ ReactiveOctocat/ReactiveOctocat/ViewModels/MainViewModel.cs
@@ -14,6 +14,7 @@ namespace ReactiveOctocat.ViewModels
     class MainViewModel : ReactiveObject
     {
         private IGitHubService _gitHubService = new GitHubMockService();
+        private MemoizingMRUCache<User, Repository[]>  _cache;
 
         public MainViewModel()
         {
@@ -28,9 +29,13 @@ namespace ReactiveOctocat.ViewModels
             this.ObservableForProperty(x => x.LoggedInUser,
                                        user => user == null ? Visibility.Hidden : Visibility.Visible).Subscribe(v => IsUserLoggedIn = v);
 
+
+            _cache = new MemoizingMRUCache<User, Repository[]>((user,_) =>
+                                                                          _gitHubService.GetRepositories(user), 3);
+
             this.WhenAny(t => t.LoggedInUser, u => u.Value != null).Where(filter => filter).Subscribe(_ =>
                 
-                    Repositories = new ReactiveCollection<Repository>(_gitHubService.GetRepositories(LoggedInUser))
+                    Repositories = new ReactiveCollection<Repository>(_cache.Get(LoggedInUser))
             );
         }
 
